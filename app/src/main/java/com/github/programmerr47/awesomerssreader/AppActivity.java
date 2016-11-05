844a86a62569940ec114624dd41e9cdd7c832179
Michael <spitsinmichael@gmail.com>
2016-11-05 14:21:45 +0300

    initial commit

---
+++ app/src/main/java/com/github/programmerr47/awesomerssreader/AppActivity.java
@@ -0,0 +1,13 @@
+package com.github.programmerr47.awesomerssreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_rss_list);
+    }
+}
